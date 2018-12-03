6e3647bcc966d203e89b139f63406c623f4cfa56
andblade <and.santos123@gmail.com>
2018-12-03 00:38:15 -0200

    progressbar, menu mobile sem transparencia

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,3 +1,37 @@
+/************************************************************************************************/
+//JavaScript do w3js para ativar os includes
+/************************************************************************************************/
+/* W3.JS 1.03 December 2017 by w3schools.com */
+"use strict";
+var w3 = {};
+
+w3.includeHTML = function(cb) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          elmnt.removeAttribute("w3-include-html");
+          w3.includeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+};
+/************************************************************************************************/
+//Fim JavaScript do w3js para ativar os includes
+/************************************************************************************************/
+
 //menu transparente com scroll
 $(function(){
   $(window).scroll(function(){
@@ -65,38 +99,4 @@ btn.on('click', function(e) {
 })();
 /************************************************************************************************/
 //Fim Exemplo de JavaScript inicial para desativar envios de formulário, se houver campos inválidos.
-/************************************************************************************************/
-
-/************************************************************************************************/
-//JavaScript do w3js para ativar os includes
-/************************************************************************************************/
-/* W3.JS 1.03 December 2017 by w3schools.com */
-"use strict";
-var w3 = {};
-
-w3.includeHTML = function(cb) {
-  var z, i, elmnt, file, xhttp;
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          elmnt.removeAttribute("w3-include-html");
-          w3.includeHTML(cb);
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
-    }
-  }
-  if (cb) cb();
-};
-/************************************************************************************************/
-//Fim JavaScript do w3js para ativar os includes
 /************************************************************************************************/
